e59289e477046cf9b0ea897b80b32f2bee505543
Tim Renouf <tpr.android@botech.co.uk>
2010-06-16 12:48:33 +0100

    Fix DiscoverServices not retrieving any records
    Patch submitted to upstream BlueZ by
    original author: Luiz Augusto von Dentz <luiz.dentz-von@nokia.com>
    
    In the case that device->tmp_records was already set, req->records was
    being set to NULL and so causes the empty array to be returned.
    
    To fix that tmp_records now always has the latest discovered records and
    it is used when repling to DiscoverServices.
    
    Thanks to Tim Renouf <tpr.vger@botech.co.uk> for figuring out where was
    the problem.
    
    Change-Id: I55e3d4d65d327d1194ec0b9860d44826d93c6de6

--- src/device.c
+++ src/device.c
@@ -1183,12 +1183,6 @@ void device_probe_drivers(struct btd_device *device, GSList *profiles)
 						g_strdup(list->data),
 						(GCompareFunc) strcasecmp);
 	}
-
-	if (device->tmp_records) {
-		sdp_list_free(device->tmp_records,
-				(sdp_free_func_t) sdp_record_free);
-		device->tmp_records = NULL;
-	}
 }
 
 static void device_remove_drivers(struct btd_device *device, GSList *uuids)
@@ -1390,12 +1384,12 @@ static void search_cb(sdp_list_t *recs, int err, gpointer user_data)
 
 	update_services(req, recs);
 
-	if (device->tmp_records && req->records) {
+	if (device->tmp_records)
 		sdp_list_free(device->tmp_records,
 					(sdp_free_func_t) sdp_record_free);
-		device->tmp_records = req->records;
-		req->records = NULL;
-	}
+
+	device->tmp_records = req->records;
+	req->records = NULL;
 
 	if (!req->profiles_added && !req->profiles_removed) {
 		debug("%s: No service update", device->path);
@@ -1422,7 +1416,7 @@ proceed:
 
 	if (dbus_message_is_method_call(req->msg, DEVICE_INTERFACE,
 					"DiscoverServices")) {
-		discover_services_reply(req, err, req->records);
+		discover_services_reply(req, err, device->tmp_records);
 		goto cleanup;
 	}
 
