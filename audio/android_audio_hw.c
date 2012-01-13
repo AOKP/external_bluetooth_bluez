d0cbf948b7c9bdc2a5a0134ea04ffcb0c825218e
Glenn Kasten <gkasten@google.com>
2012-01-13 09:31:28 -0800

    Use audio_mode_t consistently
    Change-Id: Icc0048b165032366a035d35e79a5887dccab8c73

--- audio/android_audio_hw.c
+++ audio/android_audio_hw.c
@@ -836,7 +836,7 @@ static int adev_set_master_volume(struct audio_hw_device *dev, float volume)
     return -ENOSYS;
 }
 
-static int adev_set_mode(struct audio_hw_device *dev, int mode)
+static int adev_set_mode(struct audio_hw_device *dev, audio_mode_t mode)
 {
     /* TODO: do we care for the mode? */
     return 0;
